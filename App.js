f41c7fc03cae09d5bd0245826aac5b96a2ce582f
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-25 15:17:29 -0600

    Implements basic stack navigator

--- App.js
+++ App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 
+const Stack = createNativeStackNavigator()
+
 export default function App() {
-  return (
+  const CustomComponent = () => (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+  )
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name='Pokedex'
+          component={CustomComponent}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
