0dda40636665a3ff0bf941c3a49ea0787802d522
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-26 17:06:54 -0600

    Update pokemon card component

--- src/components/pokemonCard.js
+++ src/components/pokemonCard.js
@@ -1,14 +1,18 @@
 import { View, Text, TouchableOpacity, Image } from "react-native"
+import Loading from "./loading"
 
-const PokemonCard = ({ item: pokemon }) => {
+const PokemonCard = ({
+  item: pokemon,
+  onPress = () => {}
+}) => {
+
   return (
-    <TouchableOpacity style={{ width: '45%'}} >
+    <TouchableOpacity style={{ width: '45%'}} onPress={() => onPress(pokemon.id)}>
       <View>
         <Text>{pokemon.name}</Text>
         <Image
           style={{width: 300, height: 200}}
-          // source={{ uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" }}
-          source={{ uri: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" }}
+          source={{ uri: pokemon?.sprites.front_default }}
         />
       </View>
     </TouchableOpacity>
