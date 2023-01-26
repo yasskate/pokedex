1981c7539b5418c1a2ec34d5e6231b1a9c4ff0a6
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-26 11:47:06 -0600

    Add useApi hook

---
+++ src/hooks/useApi.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react"
+
+const useApi = ({ url }) => {
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(false)
+  
+  const fetchData = async () => {
+    try {
+      const response = await (await fetch(url)).json()
+      setData(response)
+    } catch (error) {
+      setError(prevError => !prevError)
+    } finally {
+      setLoading(prevLoading => !prevLoading)
+    }  
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+  
+  return {
+    data,
+    loading,
+    // error
+  }
+}
+
+export default useApi
