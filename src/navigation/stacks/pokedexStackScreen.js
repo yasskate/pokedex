ce8ead78c5ab13124fc1662d17a0a1e672024943
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-25 19:52:13 -0600

    Add pokedex stack screens

---
+++ src/navigation/stacks/pokedexStackScreen.js
@@ -0,0 +1,33 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Pokedex from '../../screens/pokedex'
+import PokemonDetail from '../../screens/pokemonDetail'
+
+// TODO:
+// - Set pokemon name as header when detail view is shown
+// https://reactnavigation.org/docs/headers/#updating-options-with-setoptions
+
+const Stack = createNativeStackNavigator()
+
+const PokedexStackScreen = () => (
+  <Stack.Navigator initialRouteName='Pokedex'>
+    <Stack.Screen
+      name='Pokedex'
+      component={Pokedex}
+      options={{
+        headerTintColor: 'white',
+        headerStyle: {
+          backgroundColor: '#0A285F'
+        },
+        headerTitleStyle: {
+          fontWeight: 'bold'
+        }
+      }}
+    />
+    <Stack.Screen
+      name="PokemonDetail"
+      component={PokemonDetail}
+    />
+  </Stack.Navigator>
+)
+
+export default PokedexStackScreen
