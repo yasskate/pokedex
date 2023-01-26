414b08876e213968bc3a4dd92d7c1202ccf0a8a0
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-26 11:56:04 -0600

    Refactor tab navigator

--- src/navigation/tabNavigator.js
+++ src/navigation/tabNavigator.js
@@ -1,32 +1,24 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-import { createNativeStackNavigator } from '@react-navigation/native-stack'
-
 import Home from '../screens/home'
 import TabBar from '../components/tabBar'
 import PokedexStackScreen from './stacks/pokedexStackScreen'
 
 const Tab = createBottomTabNavigator()
-const Stack = createNativeStackNavigator()
 
 const TabNavigator = () => {
   return (
     <Tab.Navigator 
       tabBar={props => <TabBar {...props} />}
-      options={{ headerShown: false }}
     >
       <Tab.Screen
         name="Home"
         component={Home}
-        options={{
-          header: () => (<></>)
-        }}
+        options={{ headerTitle: 'Home' }}
         />
       <Tab.Screen
         name="Pokedex"
         component={PokedexStackScreen}
-        options={{
-          header: () => (<></>)
-        }}
+        options={{ header: () => (<></>) }}
       />
     </Tab.Navigator>
   )
