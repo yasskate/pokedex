247ca2dc20880963113dcb0f942bf65b31caf22d
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-26 23:34:16 -0600

    Update tab bar ui

--- src/navigation/tabNavigator.js
+++ src/navigation/tabNavigator.js
@@ -1,27 +1,41 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { View, StyleSheet } from 'react-native'
 import Home from '../screens/home'
 import TabBar from '../components/tabBar'
 import PokedexStackScreen from './stacks/pokedexStackScreen'
+import { colors } from '../utils/colors'
 
 const Tab = createBottomTabNavigator()
 
 const TabNavigator = () => {
   return (
-    <Tab.Navigator 
-      tabBar={props => <TabBar {...props} />}
-    >
-      <Tab.Screen
-        name="Home"
-        component={Home}
-        options={{ headerTitle: 'Home' }}
+    <View style={styles.tabContainer}>
+      <Tab.Navigator
+        tabBar={props => (<TabBar {...props} />)}
+        >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{ headerTitle: 'Home' }}
+          />
+        <Tab.Screen
+          name="Pokedex"
+          component={PokedexStackScreen}
+          options={{ header: () => (<></>) }}
         />
-      <Tab.Screen
-        name="Pokedex"
-        component={PokedexStackScreen}
-        options={{ header: () => (<></>) }}
-      />
-    </Tab.Navigator>
+      </Tab.Navigator>
+    </View>
   )
 }
+
+const styles = StyleSheet.create({
+  tabContainer: {
+    backgroundColor: colors.hardBlue,
+    flex: 1,
+    width: '100%',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 12
+  }
+})
 
 export default TabNavigator
