e651a47fde739f47b7b57b5ef65a9bde64e80f85
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-26 21:29:39 -0600

    Add reducer cases

--- src/store/reducers/pokedexReducer.js
+++ src/store/reducers/pokedexReducer.js
@@ -1,11 +1,18 @@
+// TODO:
+// - Move types to a new file 
 export const pokedexReducer = (state, action) => {
   switch (action.type) {
     case 'ADD_POKEMONS_TO_POKEDEX':
-      return { ...state, pokedex: [...action.payload]}
+      return { ...state, pokedex: [...action.payload], pokedexMainSource: [...action.payload]}
     case 'SET_POKEMON_DETAIL':
       return { ...state, pokemon: action.payload }
     case 'CLEAR_POKEMON_DETAIL':
       return { ...state, pokemon: null }
+    case 'UPDATE_QUERY_SEARCH':
+      return { ...state, querySearch: action.payload }
+    case 'SET_FILTERED_POKEMONS':
+      return { ...state, pokedex: [...action.payload] }
+    case 'RESET_FILTERED_POKEMONS':
+      return { ...state, pokedex: state.pokedexMainSource }
   }
 }
-
