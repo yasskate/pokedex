ea84e513d1251d3e99c89686a9bba12e692f5e29
Yasser Batas - yasskate <yassk8@gmail.com>
2023-01-28 17:56:42 -0600

    Improves header ui on pokedex stack

--- src/navigation/stacks/pokedexStackScreen.js
+++ src/navigation/stacks/pokedexStackScreen.js
@@ -1,6 +1,9 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import { View } from 'react-native'
 import Pokedex from '../../screens/pokedex'
 import PokemonDetail from '../../screens/pokemonDetail'
+import { colors } from '../../utils/colors'
+import { capitalizeWord } from '../../utils/utils'
 
 // TODO:
 // - Set pokemon name as header when detail view is shown
@@ -9,23 +12,35 @@ import PokemonDetail from '../../screens/pokemonDetail'
 const Stack = createNativeStackNavigator()
 
 const PokedexStackScreen = () => (
-  <Stack.Navigator initialRouteName='Pokedex'>
+  <Stack.Navigator
+    initialRouteName='Pokedex'
+    screenOptions={{
+      headerTintColor: colors.hardYellow,
+      headerStyle: {
+        fontSize: 32
+      }
+    }}
+  >
     <Stack.Screen
       name='PokedexList'
       component={Pokedex}
       options={{
-        headerTintColor: 'white',
+        title: 'Pokedex',
         headerStyle: {
-          backgroundColor: '#0A285F'
-        },
-        headerTitleStyle: {
-          fontWeight: 'bold'
+          backgroundColor: colors.hardBlue,
         }
       }}
     />
     <Stack.Screen
       name="PokemonDetail"
       component={PokemonDetail}
+      options={({ route }) => ({
+        title: `${capitalizeWord(route.params.pokemonName ?? 'Pokedex')} #${route.params.pokemonId}`,
+        headerTintColor: colors.hardBlue,
+        headerStyle: {
+          backgroundColor: colors.yellow,
+        }})
+      }
     />
   </Stack.Navigator>
 )
